6358e41743654279a5c76493399ab477adc49db2
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-09-07 09:48:10 -0700

    4-hello,puts2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
-int main()
+/*
+ * main - this function is a starting point
+ *
+ * return: return is always zero
+ */
+int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
