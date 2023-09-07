3c6609b85607fb39dd910cfa77ff222c5ea48186
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-09-07 15:14:50 -0700

    101 -quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,7 @@
 
 int main(void)
 {
-	const char* message = "and that piece of art is useful" - Dora Korpar, 2015-10-19";
+	const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	size_t len = strlen(message);
 	write(1, message, len);
 	return (1);
