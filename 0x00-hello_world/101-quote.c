ea8f246ce414c81e24082ae14751f78b0df547c6
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-09-07 15:06:54 -0700

    8-stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * main - entry point for program
+ *
+ * Return: is 1 to  print an error message to stderr
+ */
+
+int main(void)
+{
+	const char* message = "and that piece of art is useful" - Dora Korpar, 2015-10-19";
+	size_t len = strlen(message);
+	write(1, message, len);
+	return (1);
+}	
