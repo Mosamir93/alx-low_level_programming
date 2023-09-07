a257978b9f31d22071c7cb5949655efe4aeda9bc
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-09-07 15:41:45 -0700

    quote betty is horrible lol

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,8 @@
 
 int main(void)
 {
-	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	const char *message = "and that piece of art is useful\" - Dora Korpar, \
+			       2015-10-19\n";
 	size_t len = strlen(message);
 
 	write(1, message, len);
