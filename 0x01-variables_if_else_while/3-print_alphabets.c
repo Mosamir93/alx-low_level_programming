1c680b29775c5e5cdc993a2b753e608265bc3e24
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-09-08 12:47:44 -0700

    3-print

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+*main - entry point for program
+*
+*Return: return is 0 to indicate running without error
+*/
+int main(void)
+{
+	char alpha = 'a';
+	char ALPHA = 'A';
+
+	while ('a' <= 'z')
+{
+	putchar (alpha);
+	alpha++;
+}
+	while ('A' <= 'Z')
+{
+	putchar (ALPHA);
+	ALPHA++;
+}
+	putchar ('\n');
+	return (0);
+}
