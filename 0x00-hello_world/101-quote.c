1dfb783985ac4b35b47dd2804bb03950d8b70371
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-09-07 18:22:42 -0700

    101-quote.2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,8 @@
 
 int main(void)
 {
-	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char simple[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(1, quo, 59);
+	write(1, simple, 59);
 	return (1);
 }
