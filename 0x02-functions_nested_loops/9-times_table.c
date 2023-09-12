482b2f392aced0990f1fa5ae35b59d0c411ada88
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-09-12 14:24:45 -0700

    9.5

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,11 +10,12 @@ void times_table(void)
 	int i;
 	int j;
 	int k;
+
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			k = i * j
+			k = i * j;
 			_putchar(k + '0');
 			_putchar(',');
 			_putchar(' ');
