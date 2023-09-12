7aef65fec7e3f2de159ac2df0cc48c196a7c2adf
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-09-12 11:06:54 -0700

    5.3

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,12 +13,12 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	if (n == 0)
+	else if (n == 0)
 	{
 		_putchar('0');
 		return (0);
 	}
-	if (n < 0)
+	else if (n < 0)
 	{
 		_putchar('-');
 		return (-1);
