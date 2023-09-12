ba408156c8ddb35953448ac29d64bdd9711d7f3a
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-09-12 08:59:05 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *print_alphabet - prints a-z in lowercase
+ *
+ */
+
+void print_alphabet(void);
+{
+	int c;
+
+	for (c = 'a', c <= 'z', c++)
+		_putchar(c);
+		_putchar(\n);
+}
