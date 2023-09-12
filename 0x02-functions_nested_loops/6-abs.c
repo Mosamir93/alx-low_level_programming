e7837692849812291ab32608e5529eab0cbbf26f
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-09-12 12:37:03 -0700

    6.4

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,12 +3,14 @@
 /**
  *_abs - computes the absolute value of an integer
  *@i: integer to be computed
- *Return: 0 to indicate success
+ *Return: i to return the absolute value
  */
 
 int _abs(int i)
 {
 	if (i < 0)
 		i = i * (-1);
-	return (0);
+	else
+		i = i * (1);
+	return(i);
 }
