85c58ad485dbb7e799871b40b0de2a6694ea143c
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-09-12 06:35:33 -0700

    0-_putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main - entry point for control
+ *
+ *Return: 0 to indicate succes
+ */
+
+int main(void)
+{
+	printf("_putchar\n")
+	return (0)
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+#functions amd nested loops
