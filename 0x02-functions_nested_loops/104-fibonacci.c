61488c763e6558f3984014c76cd9575818a30e05
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-09-12 19:18:32 -0700

    104

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ *main - entry point for control
+ *
+ *Return: 0 to indicate succes
+ */
+
+int main(void)
+{
+	unsigned long int i1 = 1;
+	unsigned long int i2 = 2;
+	unsigned long int i3;
+	unsigned long int n;
+
+	printf("%lu, ", i1);
+	printf("%lu", i2);
+
+	for (n = 3; n <= 98; n++)
+	{
+		i3 = i1 + i2;
+		printf(", %lu", i3);
+		i1 = i2;
+		i2 = i3;
+	}
+	printf("\n");
+	return (0);
+}
