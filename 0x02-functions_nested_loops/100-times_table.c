918bfb3b2bcfe2c4b1a152b16eba230f02f8af21
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-09-12 17:30:35 -0700

    12.8

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -36,7 +36,7 @@ void print_times_table(int n)
 					_putchar((c / 10) + '0');
 					_putchar((c % 10) + '0');
 				}
-				else if (C > 99)
+				else if (c > 99)
 				{
 					_putchar((c / 100) + '0');
 					_putchar((c / 10) + '0');
