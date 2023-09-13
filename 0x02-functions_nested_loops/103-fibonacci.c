c9ef22edb5192a79f58e838786fdc80e34fcf2ed
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-09-13 16:17:37 -0700

    103

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,20 +8,24 @@
 
 int main(void)
 {
-	unsigned long int i1 = 1;
-	unsigned long int i2 = 2;
+	unsigned long int i1 = 0;
+	unsigned long int i2 = 1;
 	unsigned long int i3;
 	unsigned long int n;
-
-	printf("%lu, ", i1);
-	printf("%lu", i2);
+	unsigned long int sum;
 
 	for (n = 3; n <= 50; n++)
 	{
 		i3 = i1 + i2;
-		printf(", %lu", i3);
 		i1 = i2;
 		i2 = i3;
+		sum = 0;
+
+		if (i3 <= 4000000 && i3 % 2 == 0)
+		{
+			sum += i3
+		}
+
 	}
 	printf("\n");
 	return (0);
