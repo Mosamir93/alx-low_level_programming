cee55a07b2f6990dd6a1c0fa153d820fddaf0d27
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-09-14 10:53:12 -0700

    6-printline

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*print_line - prints a _ n times
+*
+*@n: number of times to print _ character
+*/
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = n; i <= n; i++)
+	{
+		if (i <= 0)
+		{
+			_putchar('\n');
+		}
+		_putchar('_');
+	}
+	_putchar('\n');
+
+}
