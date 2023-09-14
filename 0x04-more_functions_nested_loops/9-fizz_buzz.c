c52680545e2c03b0a67ad133b6b61a21914f3230
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-09-14 13:07:03 -0700

    9.2

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,15 +15,15 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("fizzbuzz ");
+			printf("FizzBuzz ");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("buzz ");
+			printf("Buzz ");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("fizz ");
+			printf("Fizz ");
 		}
 		else
 		{
