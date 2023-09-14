09edaf19794d0f4638c4722ad63e2df98c114ce5
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-09-14 13:00:04 -0700

    9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*main - entry point for control
+*
+*Return: 0 to indicate success
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("buzz ");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("fizzbuzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+	return (0);
+}
