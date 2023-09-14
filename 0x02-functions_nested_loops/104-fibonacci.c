8c7f8cb2c9476f494e61822b26ba6d002460ce7e
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-09-13 17:53:47 -0700

    104 -fib

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,32 +8,44 @@
 
 int main(void)
 {
-	unsigned long int i1 = 1;
-	unsigned long int i2 = 2;
-	unsigned long int i3;
+	unsigned long int i = 1;
+	unsigned long int j = 2;
+	unsigned long int k;
 	unsigned long int n;
-	unsigned long int i4;
-	unsigned long int i5;
+	unsigned long int i1;
+	unsigned long int j1;
+	unsigned long int k1;
+	unsigned long int i2;
+	unsigned long int j2;
+	unsigned long int k2;
 
-	printf("%lu, ", i1);
-	printf("%lu", i2);
+	printf("%lu, ", i);
+	printf("%lu", k);
 
-	for (n = 3; n <= 98; n++)
+	for (n = 3; n <= 88; n++)
 	{
-		i3 = i1 + i2;
-		i4 = (i1 + i2) / 10000000000;
-		i5 = (i1 + i2) % 10000000000;
-		if (n >= 75)
-		{
-			printf(", %lu%lu", i4, i5);
-		}
-		else
-		{
-			printf(", %lu", i3);
-		}
-		i1 = i2;
-		i2 = i3;
+		k = i + j;
+		printf(", %lu", k);
+		i = j;
+		j = k;
 	}
+
+	i1 = i / 1000000000
+	i2 = i % 1000000000
+	j1 = j / 1000000000
+	j2 = j % 1000000000
+
+	for (n = 89; n <= 98; n++)
+	{
+		k1 = i1 + j1;
+		k2 = i2 + j2;
+		printf(", %lu%lu", k1, k2);
+		i1 = k1 / 1000000000
+		i2 = k2 % 1000000000
+		j1 = k1 / 1000000000
+		j2 = k2 % 1000000000
+	}
+
 	printf("\n");
 	return (0);
 }
