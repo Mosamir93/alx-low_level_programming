e8d04654d61abd085429f987db975e84065ea199
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-09-18 11:25:15 -0700

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*print_rev - prints a string in reverse
+*@s: string to be printed
+*
+*/
+
+void print_rev(char *s)
+{
+
+	for (*s = '\0'; s >= 0; s--)
+		_putchar(*s);
+
+	_putchar('\n');
+}
