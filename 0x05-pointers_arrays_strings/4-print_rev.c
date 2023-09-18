5905adc467e6b471dc9fdb019dd2c00adc867b84
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-09-18 12:34:07 -0700

    4.04

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,13 +11,11 @@ void print_rev(char *s)
 	int len;
 	int i;
 
-	for (len = 0; *s != '\0'; s++)
+	for (len = 0; s[len] != '\0'; len++)
+
+	for (i = len-1; i >= 0; i--)
 	{
-		len++;
-	}
-	for (i = len; i >= 0; --i)
-	{
-		_putchar(*s[i]);
+		_putchar(s[i]);
 	}
 	_putchar('\n');
 }
