7e9db573dbf828c651667cc9ad1ec8977dcabf7a
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-09-18 13:37:24 -0700

    6.1

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,11 +8,13 @@
 
 void puts2(char *str)
 {
-	for (str = 0; *str != '\0'; str++)
+	int i;
+
+	for (i = 0; *str != '\0'; i++)
 	{
-		if (str % 2 == 0)
+		if (i % 2 == 0)
 		{
-			_putchar(*str);
+			_putchar(str[i]);
 		}
 	}
 	_putchar('\n');
