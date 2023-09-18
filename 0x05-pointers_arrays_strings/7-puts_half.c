827b261a7907a090c4a6488fdd9505e870787cfe
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-09-18 13:50:23 -0700

    7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *puts_half - prints second half of a string
+ *@str: string to be used
+ *
+ */
+
+void puts_half(char *str)
+{
+	int len;
+	int i;
+
+
+	for (len = 0; s[len] != '\0'; len++)
+	{
+	}
+	for (i = len - 1; i >= (len - 1) / 2; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n')
+}
