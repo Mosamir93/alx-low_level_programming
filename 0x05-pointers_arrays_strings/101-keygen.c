bf9086000ddb0b5ebb046d73296057500d8bb7dc
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-09-18 16:41:01 -0700

    101

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+*main - entry point for control
+*
+*Return: 0 to indicate success
+*/
+
+int main(void)
+{
+	int l;
+	char c;
+
+	srand(time(NULL));
+	for (l = 0; l <= 3000; l + c)
+	{
+		c = rand() % 128;
+		putchar(s);
+	}
+	putchar(3000 - l);
+	return (0);
+}
