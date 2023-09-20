5649964168867176a431ed72614139ec2efff9e5
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-09-20 16:08:13 -0700

    3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*_strcmp - compares two strings
+*@s1: first string to be compared
+*@s2: second string to be compared
+*Return: return the differnce
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int i, flag = 0;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			break;
+		}
+	}
+	return (s1[i] - s2[i]);
+}
