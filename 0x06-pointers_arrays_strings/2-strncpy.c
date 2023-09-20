1b5aabb69f2290eb34087569a984af481abe9e24
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-09-20 15:31:47 -0700

    2.5

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,10 +13,9 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
-	dest[i + 1] = '\0';
 	return (dest);
 }
