d7c0263f10298a9a7013d510d909f85d78a194dd
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-09-20 15:18:27 -0700

    2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *_strncpy - appends the src string to the dest string
+ *@dest: new string created
+ *@src: string to be copied
+ *@n: nimber of characters to be appended
+ *Return: dest which is a pointer to the resulting string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
