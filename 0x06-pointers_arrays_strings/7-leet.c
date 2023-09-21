e257c9b271f64fed55007d3d82c1106845d28a86
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-09-20 18:36:24 -0700

    7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *leet - encodes a string into 1337
+ *@str: string to be encoded
+ *Return: str which is string encoded
+ */
+
+char *leet(char *str)
+{
+	int i, j;
+
+	n[] = "4307143071";
+	a[] = "aeotlAEOTL";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (str[i] == a[j])
+				str[i] = n[j];
+		}
+	}
+	return (str);
+}
