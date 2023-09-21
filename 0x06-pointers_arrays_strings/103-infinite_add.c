ee5da9cfdafc8e24c37854122a0b096c2743734d
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-09-21 15:16:44 -0700

    103.03

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,37 +11,41 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, k, rem = 0, sum = 0;
+	int i, j, k, l, temp, rem = 0, sum = 0;
 
 	for (i = 0; n1[i] != '\0'; i++)
 
 	for (j = 0; n2[j] != '\0'; j++)
 
-	if (i >= j)
-		k = i;
-	else
-		k = j;
-	r[k] = '\0';
-	for (i -= 1, j -= 1, k -=1; i != 0 || j != 0; i--, j--, k--)
+	for (i -= 1, j -= 1, k = 0; i >= 0 || j >= 0; i--, j--, k++)
 	{
-		if (k < size_r - 1)
+		if (i >= 0)
+			sum += n1[i];
+		if (j >= 0)
+			sum += n2[j];
+		sum += rem;
+		if (sum > 9)
 		{
-			if (i >= 0)
-				sum += (n1[i] - '0');
-			if (j >= 0)
-				sum += (n2[j] - '0');
-			sum += rem;
-			if (sum > 9)
-			{
-				sum = sum % 10;
-				rem = sum / 10;
-			}
-			else
-				rem = 0;
-			r[k] = sum + '0';
+			rem = sum % 10;
+			sum /= 10;
 		}
 		else
-			return (0);
+			rem = 0;
+		r[k] = sum + '0';
+	}
+	if (rem != 0)
+		r[k] = rem + '0';
+	r[k + 1] = '\0';
+	if (r[k + 1] > size_r - 1)
+		return (0);
+
+	l = 0;
+	while (l <= k - 1 / 2)
+	{
+		temp = r[l];
+		r[l] = r[(k - 1) - l];
+		r[(k - 1) - l] = temp;
+	l++;
 	}
 	return (r);
 }
