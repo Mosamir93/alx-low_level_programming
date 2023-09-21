0a414353287dac0d3871c2c14c86381ed9e3a39f
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-09-21 09:39:57 -0700

    101

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_number - prints an integer
+ *@n: integer to be printed
+ */
+
+void print_number(int n)
+{
+	unsigned int x;
+
+	x = n;
+
+	if (n < 0)
+	{
+		_putchar('-')
+		x *= -1;
+	}
+	if (x / 10 != 0)
+		print_number(x / 10);
+	_putchar((x % 10) + 48);
+}
