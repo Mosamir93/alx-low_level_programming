1649442f835dafa751e9819993ee6415c59017bf
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-09-20 17:18:10 -0700

    6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *cap_string - capitalize words of a string
+ *@str: string to be changed
+ *Return: str the changed string
+ */
+
+char *cap_string(char *str)
+{
+	int i, j;
+	char *sep[13] = {',', ';', '.', '!', '?', '"',
+		'(', ')', '{', '}', '\t', '\n', ' '};
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j <= 13; j++)
+		{
+			if (str[i] == sep[j] && str[i + 1] >= 'a' && str[i + 1] <= 'z')
+			{
+				str[i + 1] -= 32;
+			}
+		}
+	}
+	return (str);
+}
