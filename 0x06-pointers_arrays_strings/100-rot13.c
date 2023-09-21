1f0aa8d4e253d761884121f95902c46a981decf4
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-09-20 21:46:01 -0700

    100

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+*rot13 - encodes a string using rot13
+*@s: string to be encoded
+*Return: s which is encoded string
+*/
+
+char *rot13(char *s)
+{
+	char a1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char a2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i, j;
+
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == a1[j])
+			{
+				s[i] = a2[j];
+			}
+
+		}
+	}
+	return (s);
+}
