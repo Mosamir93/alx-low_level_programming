b1ccbda15b1d8b78bca70857af983f68db4175a1
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-09-25 03:54:12 -0700

    2.7

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -15,9 +15,11 @@ char *_strchr(char *s, char c)
 	{
 		if (s[i] == c)
 		{
-			break;
-			return (&s[i]);
+			break;	
 		}
 	}
-	return ('\0');
+	if (s[i] == c)
+		return (&s[i]);
+	else
+		return ('\0');
 }
