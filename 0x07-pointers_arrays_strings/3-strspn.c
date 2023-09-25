3f0f28718d9a9ab2de89033dc41692afa232cd83
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-09-25 04:06:06 -0700

    3.4

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,7 +12,7 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int i, j, n;
 
 	n = 0;
-	for (i = 0; s[i] != ',' || s[i] != '\0'; i++)
+	for (i = 0; s[i] != ',' || s[i] != ';'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
