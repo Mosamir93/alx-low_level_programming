ae3f86afa6ff53c4d3ee18649f8673ebf7d7714c
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-09-25 06:46:54 -0700

    7.1

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,21 +9,22 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, j, k, sum1, sum2;
+	int i, j, sum1, sum2;
 
 	sum1 = 0;
 	for (i = 0; i < size; i++)
 	{
-		sum1 += a[i][i];
+		sum1 += a[i];
+		a += size;
 	}
 	printf("%d ,", sum1);
 
-	sum2 = k = 0;
+	sum2 = 0;
 
-	for (j = size - 1; j >= 0; j--)
+	for (j = 0; j < size; j++)
 	{
-		sum2 += a[k][j];
-		k++;
+		sum2 += a[j];
+		a -= size;
 	}
 	printf("%d", sum2);
 }
