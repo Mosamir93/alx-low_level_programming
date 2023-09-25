c251831c49924acfef66181a94e78dcf1ec1345a
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-09-25 04:11:32 -0700

    4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,13 +11,13 @@ char *_strpbrk(char *s, char *accept)
 {
 	int i, j;
 
-	for (i = 0; s[i] != accept[j] ||  s[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
-				break;
+				return (s + i);
 		}
 	}
-	return (s + i);
+	return ('\0');
 }
