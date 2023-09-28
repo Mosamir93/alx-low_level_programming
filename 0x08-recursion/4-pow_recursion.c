a431cf360abc72a85d40ad5d59e923a455455dc9
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-09-28 07:46:02 -0700

    4.2

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,6 +9,9 @@
 
 int _pow_recursion(int x, int y)
 {
+	int temp;
+
+	temp = x;
 	if (y == 0)
 		return (1);
 	if (y == 1)
@@ -16,7 +19,8 @@ int _pow_recursion(int x, int y)
 	if (y > 1)
 	{
 		_pow_recursion(x, y - 1);
-		return (x * x);
+		x = x * temp;
+		return (x);
 	}
 	return (-1);
 }
