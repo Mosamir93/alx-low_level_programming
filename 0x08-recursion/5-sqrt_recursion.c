36f368bd74dfc085caabaa70c22a23e2507a082c
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-09-28 10:00:53 -0700

    5.7

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,12 +10,12 @@ int _sqrt_recursion(int n)
 {
 	int temp;
 
-	temp = 1;
+	temp = n;
 	if (n == 1 || n == -1 || n == 0)
 		return (n);
 	else if (n > 1)
 	{
-		temp += 1;
+		temp -= 1;
 		if (temp * temp == n && temp > 1)
 			return (temp);
 		temp = (_sqrt_recursion(n));
