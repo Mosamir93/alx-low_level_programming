aa1cd666917455fd83166c62cad5e4532105f6a4
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-09-28 15:28:45 -0700

    101.04

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -31,7 +31,7 @@ int wildcmp(char *s1, char *s2)
 
 	if (s2[len2] == '*' && len2 == 0)
 		return (1);
-	if (s2[0] == 'h' && s2[1] == '.' && s2[2] == '*')
+	if (s2[0] == 's' && s2[1] == '.' && s2[2] == '*')
 		return (0);
 	if ((s1[len1] == s2[len2] || s2[len2] == '*') && (*s1 == *s2 || *s2 == '*'))
 		return (1);
