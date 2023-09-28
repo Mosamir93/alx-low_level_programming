e510a2d91845083c22b4f89219f4eb541596cbbe
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-09-28 04:22:10 -0700

    0.1

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,11 +7,11 @@
 
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	if (s[i] == '\0')
+	if (*s == '\0')
 		_putchar('\n');
 	else
-		_putchar(s[i]);
-	_puts_recursion(s[i + 1]);
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
 }
