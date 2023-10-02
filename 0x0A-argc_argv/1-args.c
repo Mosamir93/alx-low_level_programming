cb77dbca83c82a1ae66b87b5af821be8de8801f3
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-10-02 04:50:19 -0700

    1.4

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,7 +8,7 @@
 *Return: 0 to indicate success
 */
 
-int main(int argc, char **argv _attribute_((unused)))
+int main(int argc, char **argv __attribute__((unused)))
 {
 	printf("%d\n", (argc - 1));
 	return (0);
