17915b9fcdd9579d8f09751f21ba5d0da5601761
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-10-02 05:37:22 -0700

    4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - adds positive numbers
+*@argc: number of arguments
+*@argv: arguments vectoe
+*Return: 0 for success or 1 if there is a symbol in the numbers
+*/
+
+int main(int argc, char **argv)
+{
+	int i, k, sum;
+	char *c;
+
+	if (argc < 2)
+		printf("0\n");
+	sum = 0;
+	for (i = 1; i < argc; i++)
+	{
+		c = *argv + i;
+		for (k = 0; c[k] != '\0'; k++)
+		{
+			if (c[k] < '0' || c[k] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
