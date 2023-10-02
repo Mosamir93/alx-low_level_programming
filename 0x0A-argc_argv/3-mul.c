84c3c16cea4837b5712a36ecd06fd5b84279aa8a
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-10-02 05:06:56 -0700

    3.1

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,13 +8,13 @@
 *Return: 0 to indicate success or 1 if no 2 arguments recieved
 */
 
-int main(int argc, char const **argv)
+int main(int argc, char **argv)
 {
 	int mult;
 
 	if (argc == 3)
 	{
-		mult = argv[1] * argv[2];
+		mult = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", mult);
 		return (0);
 	}
