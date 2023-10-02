15f96f6bfc85ae5c0a9a2b00367984813cc8e760
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-10-02 04:00:31 -0700

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*main - prints its name, followed by a new line
+*@argc: number of arguments
+*@argv: arguments vector
+*Return: 0 to indicate success
+*/
+
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
