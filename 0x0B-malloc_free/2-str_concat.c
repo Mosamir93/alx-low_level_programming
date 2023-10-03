76f8982d45ad8678f2c11610586d805def61e4ce
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-10-03 10:12:35 -0700

    2.6

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,43 +14,28 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i, j, len1, len2;
 	char *s3;
 
+	len1 = len2 = 0;
+	for (i = 0; s1[i] != '\0'; i++)
+		len1 += 1;
+	for (j = 0; s2[j] != '\0'; j++)
+		len2 += 1;
 	if (s1 == NULL && s2 == NULL)
 		len1 = len2 = 0;
-	else
-	{
-		len1 = len2 = 0;
-		for (i = 0; s1[i] != '\0'; i++)
-			len1 += 1;
-		for (j = 0; s2[j] != '\0'; j++)
-			len2 += 1;
-	}
-	if (s1 == NULL && s2 != NULL)
-	{
-		s3 = (char *)malloc((len2 + 1) * sizeof(char));
-		for (i = 0; i < len2; i++)
-			s3[i] = s2[i];
-	}
+	else if (s1 == NULL && s2 != NULL)
+		len1 = 0;
 	else if (s2 == NULL && s1 != NULL)
-	{
-		s3 = (char *)malloc((len1 + 1) * sizeof(char));
-		for (i = 0; i < len1; i++)
-			s3[i] = s1[i];
-	}
-	else
-	{
-		s3 = (char *)malloc((len1 + len2 + 1) * sizeof(char));
-		if (s3 == NULL)
-			return (NULL);
-		for (i = 0; i < (len1 + len2); i++)
-		{
-			if (i < len1)
-				s3[i] = s1[i];
-			else
-				s3[i] = s2[i - len1];
-		}
-	}
+		len2 = 0;
+
+	s3 = (char *)malloc((len1 + len2 + 1) * sizeof(char));
 	if (s3 == NULL)
 		return (NULL);
+	for (i = 0; i < (len1 + len2); i++)
+	{
+		if (i < len1)
+			s3[i] = s1[i];
+		else
+			s3[i] = s2[i - len1];
+	}
 	s3[i] = '\0';
 	return (s3);
 }
