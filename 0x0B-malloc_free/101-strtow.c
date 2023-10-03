6c5c47b16f24ac84d704d5faf2f24a52512a308b
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-10-03 16:13:52 -0700

    101

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+*_rows - calculates number of words in a string
+*@str: string to calculate it's words
+*Return: the words in a string
+*/
+
+int _rows(char *str)
+{
+	int i, r;
+
+	r = 0;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if ((str[i] == ' ' || str[i] == '\0') && str[i - 1] != ' ')
+			r += 1;
+	}
+	return (r);
+}
+
+/**
+*_columns - counts the length of each word
+*@str: string used
+*Return: number of letters in each word
+*/
+
+int _columns(char *str)
+{
+	int i, colums;
+	static int k;
+
+	columns = 0;
+	for (i = k; str[k] != '\0'; i++)
+	{
+		if (str[i] != ' ')
+			columns += 1;
+		k++;
+		if (str[i] == ' ' && str[i - 1] != ' ')
+			break;
+	}
+	return (colums);
+}
+
+/**
+*strtow - splits a string into words
+*@str: string to be split
+*Return: pointer to an array of strings or NULL for failure
+*/
+
+char **strtow(char *str)
+{
+	int i, j, k, rows, columns;
+	char **s;
+
+	if (str == NULL || str == "")
+		return (NULL);
+	rows = _rows(str);
+	s = (char **)malloc(rows * sizeof(char *));
+	if (s == NULL)
+		return (NULL);
+	k = 0;
+	for (i = 0; i < rows; i++)
+	{
+		columns = 0;
+		columns = _columns(str);
+		s[i] = (char *)malloc((columns + 1) * sizeof(char));
+		if (s[i] == NULL)
+		{
+			for (i--; i >= 0; i--)
+				free(s[i]);
+			free(s);
+			return (NULL);
+		}
+	}
+	k = 0;
+	for (i = 0; i < rows; i++)
+	{
+		for (j = 0; str[k] != '\0'; j++)
+		{
+			if (str[k] != ' ')
+				s[i][j] = str[k];
+			k++;
+			if (str[k] == ' ' && str[k - 1] != ' ')
+				break;
+		}
+		s[i][j] = '\0';
+	}
+	return (s);
+}
