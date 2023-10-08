b70cad933fd6c412a977795b824a4eacd306b695
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-10-08 10:10:11 -0700

    101.40

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -80,10 +80,6 @@ char *_mul(char *num1, char *num2)
 	long int i, j, sum, len1, len2;
 	char *res;
 
-	if (*num1 == '\0')
-		return (NULL);
-	if (*num2 == '\0')
-		return (NULL);
 	len1 = _strlen(num1);
 	len2 = _strlen(num2);
 	res = malloc((len1 + len2 + 1) * sizeof(char));
