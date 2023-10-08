53dcddfec5ce1b53a83107a8435f7352ee17cb94
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-10-08 13:23:40 -0700

    101.50

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -9,5 +9,8 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
+void print_int(unsigned long int n);
+int _atoi(const char *s);
+void _puts(char *str);
 
 #endif
