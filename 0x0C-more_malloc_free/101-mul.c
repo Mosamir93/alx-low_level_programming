c1d6a01ab8a2939d6c49776a45586f6ff252ce7b
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-10-07 19:41:32 -0700

    101.22

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -123,12 +123,13 @@ int main(int argc, char **argv)
 	}
 	result2 = malloc((len + 1) * sizeof(char));
 	result2[len] = '\0';
-	for (i = 0; i <= len; i++, start++)
+	for (i = 0; i < len; i++, start++)
 	{
 		result2[i] = result[start];
 		_putchar(result2[i]);
 	}
 	free(result);
+	_putchar(result2[i]);
 	_putchar('\n');
 	return (0);
 }
