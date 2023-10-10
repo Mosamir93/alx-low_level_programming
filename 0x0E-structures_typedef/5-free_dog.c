2d6f47ae99402d51c5df691c1f001b59bd8e963a
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-10-10 14:27:07 -0700

    5.5

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -8,10 +8,12 @@
 
 void free_dog(dog_t *d)
 {
-	if (d->name)
-		free(d->name);
-	if (d->owner)
-		free(d->owner);
 	if (d)
+	{
+		if (d->name)
+			free(d->name);
+		if (d->owner)
+			free(d->owner);
 		free(d);
+	}
 }
