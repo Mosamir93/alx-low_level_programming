32aa417636fe53c1996c1097bd961832dfa52c54
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-10-10 14:13:34 -0700

    5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+*free_dog - frees dogs
+*@d: pointer to the structure object to be freed
+*/
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
