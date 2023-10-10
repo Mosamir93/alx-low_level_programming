456b69e59a4a4e4f615806af7a73a0aa74dbd42a
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-10-10 14:24:55 -0700

    5.0

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -2,13 +2,16 @@
 #include <stdlib.h>
 
 /**
-*free_dog - frees dogs
-*@d: pointer to the structure object to be freed
-*/
+ *free_dog - frees dogs
+ *@d: pointer to the structure object to be freed
+ */
 
 void free_dog(dog_t *d)
 {
-	(d->name) ? free(d->name) : ;
-	(d->owner) ? free(d->owner) : ;
-	(d) ? free(d) : ;
+	if (d->name)
+		free(d->name);
+	if (d->owner)
+		free(d->owner);
+	if (d)
+		free(d);
 }
