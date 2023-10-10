c29c1bf28d0127128e9b12b776a61d7aac7ec61d
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-10-10 10:10:10 -0700

    0.1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog{
+/**
+*struct dog - structure with 3 elements
+*@name: name of the dog
+*@owner: owner of the dog
+*@age: age of the dog
+*
+*Description: contains name, owner's name and age of the dog
+*/
+
+struct dog
+{
 char *name;
 char *owner;
 float age;
