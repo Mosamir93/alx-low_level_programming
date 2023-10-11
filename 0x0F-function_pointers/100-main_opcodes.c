459b6ae670b08daa8248ad562289eec3f3192d13
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-10-11 16:00:49 -0700

    100.4

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -3,27 +3,6 @@
 #include <string.h>
 
 /**
-*pr_opc - prints opcodes of it's own functiom as an array of bytes
-*@n: number of bytes
-*/
-
-void pr_opc(int n)
-{
-	unsigned char *ptr;
-	int i, fun_s;
-
-	ptr = (unsigned char *)&pr_opc;
-	fun_s = strlen((char *)ptr);
-	for (i = 0; i < n && i < fun_s; i++)
-	{
-		printf("%02hhx", ptr[i]);
-		if (i != (fun_s - 1) && i != (n - 1))
-			printf(" ");
-	}
-	printf("\n");
-}
-
-/**
 *main - entry point checks arguments count and converts it to integer
 *@argc: arguments count
 *@argv: arguments vector
@@ -32,13 +11,21 @@ void pr_opc(int n)
 
 int main(int argc, char **argv)
 {
-	int n;
+	int n, i;
+	char *ptr = (char *)main;
 
 	if (argc != 2)
 		printf("Error\n"), exit(1);
 	n = atoi(argv[1]);
 	if (n < 0)
 		printf("Error\n"), exit(2);
-	pr_opc(n);
+
+	for (i = 0; i < n; i++)
+	{
+ 		printf("%02hhx", ptr[i]);
+ 		if (i != (n - 1))
+		printf(" ");
+ 	}
+ 	printf("\n");
 	return (0);
 }
