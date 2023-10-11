fb0721f0ca5d9584f1c9debb25cb2e549a9255f9
Mosamir93 <mohamed_elshirbiny@hotmail.com>
2023-10-11 13:28:21 -0700

    3.1

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -17,7 +17,7 @@ int main(int argc, char **argv)
 		printf("Error\n"), exit(98);
 	if (get_op_func(argv[2]) == NULL)
 		printf("Error\n"), exit(99);
-	if (*(argv[3]) == 0 && (*(argv[2]) == '/' || *(argv[2]) == '%'))
+	if (!(atoi(argv[3])) && (*(argv[2]) == '/' || *(argv[2]) == '%'))
 		printf("Error\n"), exit(100);
 	result = get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3]));
 	printf("%d\n", result);
