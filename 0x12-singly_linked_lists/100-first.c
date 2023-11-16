cc753b39da99eed49fcd2fdb5680c6e4c77e732c
mohamed samir <mohamed_elshirbiny@hotmail.com>
2023-11-16 21:13:26 +0200

    100.1

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
-void _constructor(void) __attribute__ ((constructor));
+void _premain(void) __attribute__ ((constructor));
 
 /**
- * _constructor - executed before the main function
+ * _premain - executed before the main function
  * Return: void
  */
-void _constructor(void)
+void _premain(void)
 {
 	printf("You're beat! and yet, you must allow,\n"
 			"I bore my house upon my back!\n");
