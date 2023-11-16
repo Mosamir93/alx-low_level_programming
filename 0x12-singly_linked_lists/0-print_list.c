eec18b2fc6559735e260095b723c5dff84123e03
mohamed samir <mohamed_elshirbiny@hotmail.com>
2023-11-16 19:05:36 +0200

    0.3

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,7 +13,7 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		printf("[%d] %s\n", h->len, h->str ? h->str : "nil");
+		printf("[%d] %s\n", h->len, h->str ? h->str : "(nil)");
 		h = h->next;
 		count++;
 	}
