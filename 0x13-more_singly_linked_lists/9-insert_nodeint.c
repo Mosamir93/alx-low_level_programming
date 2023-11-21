37a80cbf4dd1a7755ef9495e7e92330c4dd26774
mohamed samir <mohamed_elshirbiny@hotmail.com>
2023-11-21 20:57:56 +0200

    9.1

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,7 +21,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new->n = n;
 	for (i = 0; i <= idx; i++)
 	{
-		if (p == NULL && i != idx)
+		if (p == NULL && i + 1 != idx)
 		{
 			free(new);
 			return (NULL);
