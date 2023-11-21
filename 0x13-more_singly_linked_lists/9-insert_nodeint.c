1cff85a9e5b1c7b338097e2dfd6a1be65824c006
mohamed samir <mohamed_elshirbiny@hotmail.com>
2023-11-21 21:09:05 +0200

    9.6

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,7 +20,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	p = *head;
 	new->n = n;
 	new->next = NULL;
-	if (idx == 0)
+	if (!idx)
 	{
 		new->next = p;
 		p = new;
