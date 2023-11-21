85613caf6513e03b4d2801346a0cf1ac681a341e
mohamed samir <mohamed_elshirbiny@hotmail.com>
2023-11-21 13:49:34 +0200

    5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint2 -  frees a listint_t list
+ * @head: head of the linked list
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *p = *head;
+
+	while (*head)
+	{
+		p = p->next;
+		free(*head);
+		*head = p;
+	}
+    head = NULL;
+}
