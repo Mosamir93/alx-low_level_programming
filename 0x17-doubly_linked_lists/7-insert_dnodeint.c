87f6ceb1bbd46b48b7f75828eeb07b89df3b1a10
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-01-12 00:31:30 +0200

    7.3

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -20,7 +20,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		curr = curr->next;
 	}
-	if (i != idx)
+	if (i != idx || !curr)
 		return (NULL);
 	node = malloc(sizeof(dlistint_t));
 	if (!node)
