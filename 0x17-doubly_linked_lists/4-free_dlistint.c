bce442590a68c4aaacf7bf0b939d67599d29675e
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-01-11 23:10:02 +0200

    4.1

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -3,14 +3,16 @@
 /**
  * free_dlistint - frees a dlistint_t list
  * @head: head pointer of list
-*/
+ */
 
 void free_dlistint(dlistint_t *head)
 {
-    while (head->next)
-    {
-        head = head->next;
-        free(head->prev);
-    }
-    free (head);
+	if (!head)
+		return;
+	while (head->next)
+	{
+		head = head->next;
+		free(head->prev);
+	}
+	free(head);
 }
