c29e31ae9c01c041013d8f9eb071567a34b883d8
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-01-15 19:32:32 +0200

    101.1

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Mosamir93/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libg.so
+wget -P /tmp https://github.com/Mosamir93/alx-low_level_programming/tree/d659b1ef0d10cfd4227d5956f70f40195c057d58/0x18-dynamic_libraries/libg.so
 export LD_PRELOAD=/tmp/libg.so
