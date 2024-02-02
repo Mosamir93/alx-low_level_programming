3106d1b3a3ccc86e6bca0dd8033683b7bf586478
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-02-03 01:55:48 +0200

    3.4

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,27 +11,27 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *element, *current;
+	hash_node_t *element, *current, *temp;
 
 	if (!key || !(strlen(key)) || !ht || ht->size == 0 || !(ht->array) || !(*key))
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
-	element = malloc(sizeof(hash_node_t));
+	temp = ht->array[index];
+	while (temp)
+	{
+		if (strcmp(temp->key, key) == 0)
+		{
+			free(temp->value);
+			temp->value = strdup(value);
+			if (!temp->value)
+				return (0);
+			return (1);
+		}
+		temp = temp->next;
+	}
+	element = alloc_new_node(value, key);
 	if (!element)
 		return (0);
-	element->value = strdup(value);
-	if (!element->value && value)
-	{
-		free(element);
-		return (0);
-	}
-	element->key = strdup(key);
-	if (!element->key)
-	{
-		free(element->value);
-		free(element);
-		return (0);
-	}
 	current = ht->array[index];
 	if (current == NULL)
 		ht->array[index] = element;
@@ -41,4 +41,34 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		ht->array[index] = element;
 	}
 	return (1);
+}
+
+/**
+ * alloc_new_node - allocates memory for new node
+ * @key: the key of the new node
+ * @value: value of the new node
+ * Return: the new node or NULL on failure
+ */
+
+hash_node_t *alloc_new_node(const char *value, const char *key)
+{
+	hash_node_t *element;
+
+	element = malloc(sizeof(hash_node_t));
+	if (!element)
+		return (NULL);
+	element->value = strdup(value);
+	if (!element->value && value)
+	{
+		free(element);
+		return (NULL);
+	}
+	element->key = strdup(key);
+	if (!element->key)
+	{
+		free(element->value);
+		free(element);
+		return (NULL);
+	}
+	return (element);
 }
