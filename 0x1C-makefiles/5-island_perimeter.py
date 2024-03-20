26eb43f21ca12d3fa2f64cf123ba4e6a54ce4755
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-03-21 01:21:50 +0200

    5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Island Perimeter module."""
+
+
+def island_perimeter(grid):
+    """Returns the perimeter of the island described in grid."""
+    perimeter = 0
+    rows, columns = len(grid), len(grid[0])
+    for i in range(rows):
+        for j in range(columns):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+    return perimeter
