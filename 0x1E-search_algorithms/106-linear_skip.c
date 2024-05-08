8030bf1262888ed0f9ff225bbee9704308e9ace4
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-05-08 07:26:02 +0300

    task-106.2

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -15,7 +15,7 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 
 	if (list == NULL)
 		return (NULL);
-	while (express && express->express && express->n <= value)
+	while (express && express->express && express->n < value)
 	{
 		prev = express;
 		express = express->express;
